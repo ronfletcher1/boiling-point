77b2c4f07307f240180fe2259ec7084102448f8a
ronfletcher1 <ronfletcher1@me.com>
2019-02-19 13:01:56 -0500

    first commit new react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import TemperatureApp from './TemperatureApp'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TemperatureApp />
+      </div>
+    );
+  }
+}
+
+export default App;
